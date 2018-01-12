bc57ccd5738cc5ef1b3808d0e9236e891d155594
NorthDroid away <jorundtm@gmail.com>
2018-01-12 20:39:39 +0100

    Initial commit

---
+++ imagegenerator/src/main/java/com/northdroid/imagegenerator/Image.java
@@ -0,0 +1,14 @@
+package com.northdroid.imagegenerator;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by jorundm on 12.01.2018.
+ */
+
+public class Image {
+    public Bitmap render(){
+
+        return new Bitmap.createBitmap(R.drawable.sample);
+    }
+}
